842dccb168afb8377a1acb8550bb55732c1c82ab
Ricardo Canales <canalesb93@gmail.com>
2014-11-22 21:14:15 -0600

    ASDFASDF

--- Controles/ControlRegistro.java
+++ Controles/ControlRegistro.java
@@ -2,10 +2,9 @@ package controles;
 import entidades.*;
 
 public class ControlRegistro {
-    Cliente suscriptor;
+    Cliente suscriptor = new Cliente();
     
     public void ControlRegistro() {
-        suscriptor = new Cliente();
     }
     
     public void agregarCliente(String email, String nombre, String apellido, String telefono, String password) {
