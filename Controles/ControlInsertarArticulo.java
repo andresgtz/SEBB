e10fab7e5410ff805489d8fdf632efac7be389b0
Ricardo Canales <canalesb93@gmail.com>
2014-11-24 17:59:05 -0600

    Agrega insertar articulo

---
+++ Controles/ControlInsertarArticulo.java
@@ -0,0 +1,18 @@
+package controles;
+import entidades.Articulo;
+
+// CANALES - RECIBE: int idArticulo, int idRevista, string nombre, string autor, string fecha, string autor, string clasificacion
+// aqui se crea un articulo nuevo
+
+public class ControlInsertarArticulo {
+  Articulos articulo;
+
+  // public ControlPublicarArticulo() {
+  //   articulo = new Articulo();
+  // }
+
+  // public void publicarArticulo(int idArticulo, String fecha) {
+  //   articulo.publicarArticulo(idArticulo, fecha);
+  // }
+}
+
