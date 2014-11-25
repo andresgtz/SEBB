64a40fe95b19b769dccdf6c41aa63c639f36b744
Jesus Navarro <jesusanavarrom@gmail.com>
2014-11-25 12:47:24 -0600

    Add interfaz votar

---
+++ Interfaces/InterfazVotar.java
@@ -0,0 +1,83 @@
+package interfaces;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+import java.util.*;
+import controles.ControlAbrirArticulo;
+
+public class InterfazAbrirArticulo extends HttpServlet {
+  HttpServletResponse thisResponse;
+  HttpServletRequest thisRequest;
+  PrintWriter out;
+  ControlVotar cr;
+
+  public void doGet(HttpServletRequest request,
+      HttpServletResponse response)
+      throws IOException {
+    thisResponse = response;
+    thisRequest = request;
+    thisResponse.setContentType("text/html");
+    out = thisResponse.getWriter();
+
+    out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
+    out.println("<HTML>");
+    out.println("<HEAD>");
+    out.println("<script src='//code.jquery.com/jquery-2.1.1.min.js'></script>");//h
+    out.println("<link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet'>");//h
+    out.println("<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>");//h
+    out.println("<META http-equiv=Content-Type content=\"text/html\">");
+    out.println("</HEAD>");
+    out.println("<BODY>");
+    out.println("<div class='jumbotron'>");//o
+    out.println("<div class='container' style='text-align: center;'>");//o
+    out.println("<TITLE>SEBB</TITLE>");
+    out.println("<h1>Articulo</h1>");
+    out.println("<h3>Votar</h3>");
+
+    String operacion = request.getParameter("operacion");
+    if(operacion == null){
+      pedirDatos();
+    } else if(operacion.equals("Votar")){
+      buscarArticulo();
+    }
+
+  }
+
+  public void pedirDatos(){
+    out.println("<p>Indique el ID del articulo a Votar</p>");
+    out.println("<form method=\"GET\" action=\"Votar\">");
+    out.println("<input type=\"hidden\" name=\"operacion\" value=\"Votar\"/>");
+    out.println("<p> ID Articulo  <input type=\"text\" name=\"idArticulo\" size=\"15\"></p>");
+    out.println("<p><input type=\"submit\" value=\"Ver\"name=\"B1\"></p>");
+    out.println("</form>");
+
+    out.println("<form method=\"GET\" action=\"menu.html\">");
+    out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
+    out.println("</form>");
+
+    out.println("</div>");//c
+    out.println("</div>");//c
+    out.println("</BODY>");
+    out.println("</HTML>");
+  }
+
+  public void buscarArticulo() {
+    cr = new ControlVotar();
+
+    int idArticulo = Integer.parseInt(thisRequest.getParameter("idArticulo").trim());
+    cr.Votar(idArticulo);
+    out.println("<p>Has votado por el Articulo: "+idArticulo+"</p>");
+
+    
+    out.println("<p>Presione el boton para terminar.</p>");
+    out.println("<form method=\"GET\" action=\"menu.html\">");
+    out.println("<p><input type=\"submit\" value=\"Terminar\"name=\"B1\"></p>");
+    out.println("</form>");
+    out.println("</div>");//c
+    out.println("</div>");//c
+    out.println("</BODY>");
+    out.println("</HTML>");
+  }
+
+}
+
