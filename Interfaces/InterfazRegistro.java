48d214f24f97e90bde32c2bd63360f8fcce1caa5
Jesus Navarro <jesusanavarrom@gmail.com>
2014-11-25 12:35:15 -0600

    Correccion de Headers

--- Interfaces/InterfazRegistro.java
+++ Interfaces/InterfazRegistro.java
@@ -32,7 +32,7 @@ public class InterfazRegistro extends HttpServlet {
     out.println("<div class='container' style='text-align: center;'>");//o
     out.println("<TITLE>SEBB</TITLE>");
 
-    out.println("<h1>Revista</h1>");
+    out.println("<h1>Nuevo Usuario</h1>");
     out.println("<h3>Registro</h3>");
 
     String operacion = request.getParameter("operacion");
