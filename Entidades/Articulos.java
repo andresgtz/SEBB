ba4cf623d079238f51204343c2d5f345792886c6
mrquorr <sergio.93.op@gmail.com>
2014-11-25 02:17:05 -0600

    se agrego votar

--- Entidades/Articulos.java
+++ Entidades/Articulos.java
@@ -24,5 +24,16 @@ Statement stmt;
          return rs;
       } catch (SQLException x) {System.out.println("No se encontro el articulo " + x);}
     }
+    
+    public void votar(int idArticulo) {
+      try {
+        stmt.executeQuery ("SELECT votos FROM Articulos WHERE idArticulo =" + idArticulo);
+        ResultSet rs = stmt.getResultSet();
+        int votos = rs.getInt ("votos");
+        votos = votos + 1;
+        String s = "UPDATE Articulos SET votos = " + votos + " WHERE idArticulo = " + idArticulo;
+        stmt.executeQuery (s);
+      } catch (SQLException e) {System.out.println ("Cannot vote for the article" + e);}
+    }
 
 }
