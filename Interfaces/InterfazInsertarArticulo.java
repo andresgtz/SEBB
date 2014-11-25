62bd309a6148060af13c617399832a52f7ee8688
Ricardo Canales <canalesb93@gmail.com>
2014-11-25 12:49:10 -0600

    ASDFa

--- Interfaces/InterfazInsertarArticulo.java
+++ Interfaces/InterfazInsertarArticulo.java
@@ -32,7 +32,7 @@ public class InterfazInsertarArticulo extends HttpServlet {
     out.println("<div class='container' style='text-align: center;'>");//o
     out.println("<TITLE>SEBB</TITLE>");
     out.println("<h1>Articulo</h1>");
-    out.println("<h3>Registro</h3>");
+    out.println("<h3>Crear</h3>");
 
     String operacion = request.getParameter("operacion");
     if(operacion == null){
