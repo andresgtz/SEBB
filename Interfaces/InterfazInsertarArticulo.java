85b7044a99c80da6af2043e02a63c7672320296d
Jesus Navarro <jesusanavarrom@gmail.com>
2014-11-25 01:02:03 -0600

    ins art

--- Interfaces/InterfazInsertarArticulo.java
+++ Interfaces/InterfazInsertarArticulo.java
@@ -26,7 +26,7 @@ public class InterfazInsertarArticulo extends HttpServlet {
     out.println("</HEAD>");
     out.println("<BODY>");
     out.println("<TITLE>SEBB</TITLE>");
-    out.println("<h1>Revista</h1>");
+    out.println("<h1>Articulo</h1>");
     out.println("<h3>Registro</h3>");
 
     String operacion = request.getParameter("operacion");
