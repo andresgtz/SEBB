6197fc6936a42e486b6d776e202ff23c630f9169
Ricardo Canales <canalesb93@gmail.com>
2014-11-25 02:06:56 -0600

    Agrega cosas de registro

--- Interfaces/InterfazRegistro.java
+++ Interfaces/InterfazRegistro.java
@@ -22,10 +22,16 @@ public class InterfazRegistro extends HttpServlet {
     out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\">");
     out.println("<HTML>");
     out.println("<HEAD>");
+    out.println("<script src='//code.jquery.com/jquery-2.1.1.min.js'></script>");//h
+    out.println("<link href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css' rel='stylesheet'>");//h
+    out.println("<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js'></script>");//h
     out.println("<META http-equiv=Content-Type content=\"text/html\">");
     out.println("</HEAD>");
     out.println("<BODY>");
+    out.println("<div class='jumbotron'>");//o
+    out.println("<div class='container'> style='text-align: center;'");//o
     out.println("<TITLE>SEBB</TITLE>");
+
     out.println("<h1>Revista</h1>");
     out.println("<h3>Registro</h3>");
 
@@ -48,12 +54,16 @@ public class InterfazRegistro extends HttpServlet {
     out.println("<p> Telefono  <input type=\"text\" name=\"telefono\" size=\"15\"></p>");
     out.println("<p> Password <input type=\"text\" name=\"password\" size=\"15\"></p>");
     out.println("<p> Tipo <input type=\"text\" name=\"tipo\" size=\"15\"></p>");
+    out.println("<p>Tipos:<br><ul><li>1: Suscriptor</li><li>2: Autor</li><li>3: Juez</li><li>4: Jefe Editor</li></li></p>");
     out.println("<p><input type=\"submit\" value=\"Registrar\"name=\"B1\"></p>");
     out.println("</form>");
  
     out.println("<form method=\"GET\" action=\"index.html\">");
     out.println("<p><input type=\"submit\" value=\"Cancelar\"name=\"B2\"></p>");
     out.println("</form>");
+
+    out.println("</div>");//c
+    out.println("</div>");//c
 
     out.println("</BODY>");
     out.println("</HTML>");    
@@ -81,6 +91,8 @@ public class InterfazRegistro extends HttpServlet {
     out.println("<form method=\"GET\" action=\"index.html\">");
     out.println("<p><input type=\"submit\" value=\"Terminar\"name=\"B1\"></p>");
     out.println("</form>");
+    out.println("</div>");//c
+    out.println("</div>");//c
     out.println("</BODY>");
     out.println("</HTML>"); 
   }
